3dab34a53b177379cbed0868ca6b4c690fd6d6a2
songyan <2228312644@qq.com>
2020-06-07 12:29:33 +0800

    初始化

---
+++ src/main/java/com/kit/api/KitApiApplication.java
@@ -0,0 +1,15 @@
+package com.kit.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ASUS
+ */
+@SpringBootApplication
+public class KitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kit/api/KitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
