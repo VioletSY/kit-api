b30607cec09dc5e3ca8703c2a5566c5732da3210
songyan <2228312644@qq.com>
2020-06-07 12:30:07 +0800

    初始化

--- src/main/java/com/kit/api/KitApiApplication.java
+++ src/main/java/com/kit/api/KitApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class KitApiApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(KitApiApplication.class, args);
     }
